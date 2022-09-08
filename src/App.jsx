286cdf1151a3b05a6ede218687715310de3817b1
Duminda Kuruppu <kuruppu.duminda@gmail.com>
2022-09-08 17:54:33 +0530

    Default buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import './App.css';
-import { PrimaryButton } from '@fluentui/react';
-import { useState } from 'react';
+import "./App.css";
 
+import { DefaultButton, PrimaryButton } from "@fluentui/react/lib/Button";
+import { FontSizes } from "@fluentui/theme";
+import { Icon } from "@fluentui/react/lib/Icon";
+import { initializeIcons } from "@fluentui/font-icons-mdl2";
+
+initializeIcons();
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
-        <p>Click the Button</p>
-        <PrimaryButton text="Primary" onClick={_alertClicked} />
+
+        <br />
+        <Icon
+          iconName="WindowsLogo"
+          style={{ fontSize: FontSizes.size68, color: "#0078d4" }}
+        />
+        <br />
+
+        <div>
+          <DefaultButton className="btn">Default Button</DefaultButton>
+          <PrimaryButton className="btn">Primary Button</PrimaryButton>
+        </div>
       </header>
     </div>
   );
-}
-
-function _alertClicked() {
-  alert('Clicked');
 }
 
 export default App;
